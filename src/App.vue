0d4a1dcee97b441d2aa18fcc9699b05c8cbf4d7f
lkl <1691487896@qq.com>
2026-06-19 00:37:00 +0800

    feat: 创建矿区以及分发30颗矿石

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,45 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
+import { ref } from 'vue';
+import type { Cell } from './types/game';
+// 矿区-装载着255个不确定状态的金矿
+const grid = ref<Cell[]>([])
+const BOARD_SIZE = 15; // 一行 15 个格子
+// 随机分发矿石
+const generatePrizes = () => {
+  let count = 0 //记录发出去的矿石
+  while (count < 30) {
+    // 随机生成一个 0 到 224 的索引号
+    const randomIndex = Math.floor(Math.random() * 225)
+    if (grid.value[randomIndex].type === '') {
+      grid.value[randomIndex].type = 'success'
+      grid.value[randomIndex].score = Math.floor(Math.random() * 3) + 1
+      count++
+    }
+  }
+  // 其余金矿的状态为error
+  grid.value.forEach((item) => {
+    if (item.type === '') {
+      item.type = 'error'
+    }
+  })
+
+}
+// 创建矿区-矿石初始化
+const initBoard = () => {
+  const tempBoard: Cell[] = []
+  for (let i = 0; i < BOARD_SIZE * BOARD_SIZE; i++) {
+    const row = Math.floor(i / BOARD_SIZE)
+    const col = i % BOARD_SIZE
+    tempBoard.push({ id: i, row, col, type: '', mined: false, score: 0 })
+  }
+  grid.value = tempBoard
+  // 开始分发矿石
+  generatePrizes()
+  console.log(grid.value);
+}
+initBoard()
 </script>
 
 <template>
-  <HelloWorld />
+
 </template>
---
+++ src/types/game.ts
@@ -0,0 +1,33 @@
+
+/**
+ * 矿区格子的状态类型
+ * - 'success': 成功挖到宝贝
+ * - 'error': 没挖到、空地或触发惩罚
+ * - '': 未初始化的初始状态
+ */
+export type CallType = 'success' | 'error' | ''
+
+/**
+ * 单个格子的完整数据接口
+ */
+export interface Cell {
+    /** 格子的唯一标识，从 0 开始递增 */
+    id: number;
+
+    /** 格子所在的行坐标（0-14） */
+    row: number;
+
+    /** 格子所在的列坐标（0-14） */
+    col: number;
+
+    /** * 当前格子的奖惩属性 
+     * @example 'success' | 'error' | ''
+     */
+    type: CallType;
+
+    /** 该格子是否已经被玩家点击挖开了 */
+    mined: boolean;
+
+    /** 挖到该格子可以获得的分数奖励 */
+    score: number;
+}
